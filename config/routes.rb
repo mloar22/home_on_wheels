6a9deb40a2ccdfe0bfc3031fa8c6fca359b5a4e0
Mitch Loar <mitchloarweb@gmail.com>
2017-03-22 02:02:41 -0600

    added a small menu bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   resources :homes
-  get 'welcome/index'
-  get 'pricing' => 'sell#pricing'
 
+  # pricing_path
+  get 'pricing' => 'sell#pricing'
+  # root_path
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
